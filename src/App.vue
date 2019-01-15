a31ec7c4e43a49c013b7a1b84830feeefb91a23f
renebitter <renebitter@gmail.com>
2019-01-15 14:32:05 +0100

    First Commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,51 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container mb-5">
+    <div class="card">
+      <!--<img alt="Vue logo" src="./assets/logo.png">-->
+
+      <Header text="Reverse App"/>
+
+      <Content msg="Content"/>
+
+      <Footer text="Footer"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Content from './components/Content.vue'
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import './assets/app.css';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    Content,
+    Header,
+    Footer
   }
 }
 </script>
 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    h3 {
+        margin: 40px 0 0;
+    }
+    ul {
+        list-style-type: none;
+        padding: 0;
+    }
+    li {
+        /*display: inline-block;*/
+        margin: 0 10px;
+    }
+    a {
+        color: #42b983;
+    }
+    .container{
+     width: 340px;
+    }
 </style>
